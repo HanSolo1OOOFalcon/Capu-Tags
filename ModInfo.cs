1afd04a9136c2e332589ac6962d0da7283fb192a
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-06-25 00:06:21 +0200

    Forgot to change the version in ModInfo...

--- ModInfo.cs
+++ ModInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "hansolo1000falcon.caputags";
         public const string Name = "CapuTags";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
 }
