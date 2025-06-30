e1215c4caec42cd49cdc254b89595607ed9d099c
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-06-30 22:39:09 +0200

    veresion

--- CapuTags/ModInfo.cs
+++ CapuTags/ModInfo.cs
@@ -2,7 +2,7 @@ namespace CapuTags
 {
     public class ModInfo
     {
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.3";
         public const string NAME = "CapuTags";
         public const string AUTHOR = "HanSolo1000Falcon";
     }
